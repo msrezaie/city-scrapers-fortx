ae0667de14e48cf28f58c6c4e9695834f26aaac6
msrezaie <79583632+msrezaie@users.noreply.github.com>
2024-12-19 19:46:46 -0500

    spider for Fort Worth Public Meetings

---
+++ city_scrapers/spiders/fortx_Fort_Worth_Public_Meetings.py
@@ -0,0 +1,118 @@
+import json
+from datetime import datetime
+
+import scrapy
+from city_scrapers_core.constants import CITY_COUNCIL
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+from dateutil.parser import parse as dateparse
+
+
+class FortxFortWorthPublicMeetingsSpider(CityScrapersSpider):
+    name = "fortx_Fort_Worth_Public_Meetings"
+    agency = "Fort Worth Public Meetings"
+    timezone = "America/Chicago"
+    custom_settings = {
+        "ROBOTSTXT_OBEY": False,
+    }
+
+    main_url = "https://www.fortworthtexas.gov/"
+
+    meetings_url = "https://www.fortworthtexas.gov/ocapi/calendars/getcalendaritems"
+
+    meetings_url_payload = {
+        "LanguageCode": "en-US",
+        "Ids": ["8efac0b6-9ea3-402e-b7d9-e9e71a2a34a0"],
+        "StartDate": "",
+        "EndDate": "",
+    }
+
+    meeting_detail_url = (
+        "https://www.fortworthtexas.gov/ocapi/get/contentinfo"
+        "?calendarId={calendarId}&contentId={contentId}&language=en-US"
+        "&currentDateTime={currentDateTime}&mainContentId={mainContentId}"
+    )
+
+    def start_requests(self):
+        """
+        The meeting items for this organization are
+        fetched from two of its API endpoints. The
+        main API endpoint allows fetching meeting
+        items for the entirety of one year. The
+        spider is set to fetch all meetings for the
+        current year.
+        """
+        current_date = datetime.now().date()
+
+        start_of_year = datetime(current_date.year, 1, 1).date()
+        end_of_year = datetime(current_date.year, 12, 31).date()
+
+        self.meetings_url_payload["StartDate"] = str(start_of_year)
+        self.meetings_url_payload["EndDate"] = str(end_of_year)
+
+        yield scrapy.Request(
+            url=self.meetings_url,
+            method="POST",
+            body=json.dumps(self.meetings_url_payload),
+            headers={"Content-Type": "application/json"},
+            callback=self.parse,
+        )
+
+    def parse(self, response):
+        data = response.json()
+
+        items = []
+        for meeting in data["data"]:
+            items.extend(meeting["Items"])
+
+        for item in items:
+            date_obj = datetime.strptime(item["DateTime"], "%d/%m/%Y %I:%M:%S %p")
+            currentDateTime = date_obj.strftime("%d/%m/%Y%%20%I:%M:%S%%20%p")
+
+            meeting_detail_url = self.meeting_detail_url.format(
+                calendarId=item["CalendarId"],
+                contentId=item["Id"],
+                currentDateTime=currentDateTime,
+                mainContentId=item["MainContentId"],
+            )
+
+            yield scrapy.Request(
+                url=meeting_detail_url,
+                method="GET",
+                callback=self.parse_meeting,
+                cb_kwargs={"item": item},
+            )
+
+    def parse_meeting(self, response, item):
+        data = response.json()
+        meeting_data = data["data"]
+
+        meeting = Meeting(
+            title=meeting_data["Title"],
+            description=self._parse_description(meeting_data),
+            classification=CITY_COUNCIL,
+            start=dateparse(item["DateTime"]),
+            end=None,
+            all_day=False,
+            time_notes="Please check the meeting description for details on the start time",  # noqa
+            location=self._parse_location(meeting_data),
+            links=[],
+            source=response.url,
+        )
+
+        meeting["status"] = self._get_status(meeting)
+        meeting["id"] = self._get_id(meeting)
+
+        yield meeting
+
+    def _parse_description(self, item):
+        description = item["Description"]
+        description = description.replace("\r", "").replace("\n", "")
+        return description
+
+    def _parse_location(self, item):
+        """Meetings which are cancelled do not return the full address"""
+        address = item["Address"]
+        if item["IsCancelled"]:
+            return {"name": address["Suburb"], "address": address["Formatted"].strip()}
+        return {"name": address["Venue"], "address": address["Formatted"].strip()}
---
+++ tests/test_fortx_Fort_Worth_Public_Meetings.py
@@ -0,0 +1,127 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest
+import scrapy
+from city_scrapers_core.constants import CITY_COUNCIL
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.fortx_Fort_Worth_Public_Meetings import (
+    FortxFortWorthPublicMeetingsSpider,
+)
+
+meetings_items = file_response(
+    join(
+        dirname(__file__),
+        "files",
+        "fortx_Fort_Worth_Public_Meetings_meeting_items.json",
+    ),
+    url="https://www.fortworthtexas.gov/ocapi/calendars/getcalendaritems",
+)
+
+meetings_detail = file_response(
+    join(
+        dirname(__file__),
+        "files",
+        "fortx_Fort_Worth_Public_Meetings_meeting_details.json",
+    ),
+    url=(
+        "https://www.fortworthtexas.gov/ocapi/get/contentinfo?calendarId=8efac0b6-9ea3-402e-b7d9-e9e71a2a34a0"  # noqa
+        "&contentId=9c1c703d-0beb-46b1-9776-4bd79b26cefc&language=en-US&currentDateTime=01/02/2024%2006:00:00%20PM"  # noqa
+        "&mainContentId=00000000-0000-0000-0000-000000000000"
+    ),
+)
+
+spider = FortxFortWorthPublicMeetingsSpider()
+
+freezer = freeze_time("2024-12-19")
+freezer.start()
+
+parsed_items = []
+for req in spider.parse(meetings_items):
+    if isinstance(req, scrapy.Request):
+        meeting_detail_item = spider.parse_meeting(
+            meetings_detail, req.cb_kwargs["item"]
+        )
+        parsed_items.extend(meeting_detail_item)
+
+freezer.stop()
+
+"""
+The spider for this site is set to fetch meeting items for the entire year.
+To make the test less time consuming, the number of meetings to be tested is
+limited to 6 items.
+"""
+
+
+def test_count():
+    assert len(parsed_items) == 6
+
+
+def test_title():
+    assert parsed_items[0]["title"] == "TPW Meeting Glasgow and Oak Grove Roads"
+
+
+def test_description():
+    assert parsed_items[0]["description"] == (
+        "Join us at the upcoming public meeting to learn about street upgrades. "
+        "This project is located in Council District 8.The City of Fort Worth "
+        "Transportation and Public Works (TPW) Department will present upcoming "
+        "street improvements at theHighland Hills Neighborhood Association meeting."
+        "Thursday, February 1, 2024 at 6:00 p.m."
+    )
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2024, 1, 2, 18, 0)
+
+
+def test_end():
+    assert parsed_items[0]["end"] is None
+
+
+def test_time_notes():
+    assert (
+        parsed_items[0]["time_notes"]
+        == "Please check the meeting description for details on the start time"
+    )
+
+
+def test_id():
+    assert parsed_items[0]["id"] == (
+        "fortx_Fort_Worth_Public_Meetings/202401021800/x/"
+        "tpw_meeting_glasgow_and_oak_grove_roads"
+    )
+
+
+def test_status():
+    assert parsed_items[0]["status"] == "passed"
+
+
+def test_location():
+    assert parsed_items[0]["location"] == {
+        "name": "Highland Hills Community Center",
+        "address": "Highland Hills Community Center, 1600 Glasgow Road, Fort Worth, 76134",  # noqa
+    }
+
+
+def test_source():
+    assert parsed_items[0]["source"] == (
+        "https://www.fortworthtexas.gov/ocapi/get/contentinfo?calendarId=8efac0b6-9ea3-402e-b7d9-e9e71a2a34a0"  # noqa
+        "&contentId=9c1c703d-0beb-46b1-9776-4bd79b26cefc&language=en-US&currentDateTime=01/02/2024%2006:00:00%20PM"  # noqa
+        "&mainContentId=00000000-0000-0000-0000-000000000000"
+    )
+
+
+def test_links():
+    assert parsed_items[0]["links"] == []
+
+
+def test_classification():
+    assert parsed_items[0]["classification"] == CITY_COUNCIL
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_all_day(item):
+    assert item["all_day"] is False
